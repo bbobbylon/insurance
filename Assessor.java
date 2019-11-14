071f5941e6009e06b6636ef1c72a75e528fbc31a
Bobby Oliver <46685513+bbobbylon@users.noreply.github.com>
2019-11-14 12:33:58 -0600

    Assessor Updated
    First half of methods are now commented with Javadocs format.

--- Assessor.java
+++ Assessor.java
@@ -1,12 +1,26 @@
 import java.util.ArrayList;
-public class Assessor {
 
+/**
+ * @author bobby
+ *This class is used for the calculations behind assessing a member(new or old)
+ *It has a class for each parameter that is considered when scoring a member:
+ *age, bmi, blood pressure, and family disease. There are also two classes that
+ *calculate bmi and blood pressure using a simple conversion formula (since we are
+ *converting from lbs to kgs and height in ft to height in m.
+ */
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Here are the insurance assessments: ");
-	}
-
+public class Assessor
+{
+	
+/**
+ * This method simply returns an int depending on the member's age
+ * @param age is an int that compared to 30, 45, or 60, and they 
+ * respectfully return a score to be added with all the other scores to find
+ * an insurance rate.
+ * @return 0, 10, 20, or 30. These are points that will be added to the total score,
+ * which determines a member's insurance rate plan.
+ */
+	
 public int ageScore(int age)
 	{
 	if (age<30)
@@ -27,139 +41,150 @@ public int ageScore(int age)
 	}
 	}
 
-public String bmiScore(String bmi)
+/**
+ * This method does the actual calculations and conversions from lb to kg and ft to m.
+ * @param height is an integer that is divided by 39.37 to convert to meters.
+ * @param weight is an integer that is divided by 2.205 to convert to kgs.
+ * @return bmi, which is weight divided by height squared. This determines a score
+ * range for a member and is added to the total score, which gives a member an
+ * insurance rate based on their total score.
+ */
+
+public double calcBmiScore(int height, int weight)
+{
+	//one lb = 0.453592 kilos
+	//one ounce = 0.0283495 kilos
+	//one foot = 0.3048 meters
+	//one inch = 0.0254 meters
+	//weight in kilos / height in meters squared = bmi
+	
+	double bmi = (weight/2.205) / Math.pow(height/39.37, 2) ;
+	return bmi;
+}
+
+/**
+ * This method takes in a string called bmi and returs an int.
+ * @param bmi this parameter is used as a string because of the given text file
+ * @return
+ */
+
+public int bmiScore(String bmi)
 {
 	if (bmi.contentEquals("normal"))
 	{
-		return "normal";
+		return 0;
 	}
 	else if (bmi.contentEquals("overweight"))
 	{
-		return "overweight";
+		return 30;
 	}
 	else
 	{
-		return "else";
+		return 75;
 	}
 }
 
-public String bloodPressureScore(String score)
+/**
+ * 
+ * @param BPSys
+ * @param BPDias
+ * @return
+ */
+public String calcBloodPressure(int BPSys, int BPDias)
 {
-	if (score.contentEquals("normal"))
+	if (120<=BPSys && 80<= BPDias)
 	{
-		return "0 points";
+		return "normal";
 	}
-	else if (score.contentEquals("elevated"))
+	else if (120<=BPSys&& BPSys<=129 && 80<= BPDias)
 	{
-		return "15 points";
+		return "elevated";
 	}
-	else if (score.contentEquals("stage 1"))
+	else if (130<=BPSys&& BPSys<=139 && 80<= BPDias && BPDias<=89)
 	{
-		return "30 points";
+		return "stage 1";
 	}
-	else if (score.contentEquals("stage 2"))
+	else if (BPSys>=140 &&BPDias>=90)
 	{
-		return "75 points";
+		return "stage 2";
+	}
+	else if (BPSys>=180 && BPDias>=120)
+	{
+		return "crisis";
+	}
+	else
+		return null;
+}
+public int bloodPressureScore(String score, int BPSys, int BPDias)
+{
+	if (calcBloodPressure(BPSys, BPDias).equals("normal"))
+	{
+		return 0;
+	}
+	else if (calcBloodPressure(BPSys, BPDias).equals("elevated"))
+	{
+		return 15;
+	}
+	else if (calcBloodPressure(BPSys, BPDias).equals("stage 1"))
+	{
+		return 30;
+	}
+	else if (calcBloodPressure(BPSys, BPDias).equals("stage 2"))
+	{
+		return 75;
 	}
 	else 
 	{
-		return "100 points";
+		return 100;
 	}
 	
 }
 
+/**
+ * 
+ * @param disease
+ * @return
+ */
 
-	public String familyDiseaseScore(String disease)
+	public int familyDiseaseScore(String disease)
 	{
 		if (disease.contentEquals("diabetes"))
 		{
-			return "10 points";
+			return 10;
 		}
 		if (disease.contentEquals("cancer"))
 		{
-			return "10 points";
+			return 10;
 		}
 		else
 		{
-			return "10 points";
+			return 10;
 		}
 	}
+
+/**
+ * 	
+ * @param mem
+ * @return
+ */
 	
-public String overallScore(InsuranceScore scores)
+//public ArrayList<InsuranceScore> assessMember(Member mem)
+public int assessMember(Member mem)
 {
 	String total = "0";
-	ArrayList<String> iScore = scores.getScore();
-	for (String score:iScore)
-	{
-		total = total + score;
-	}
-	return " ";
+	int age = mem.getAge();
+	int weight = mem.getWeight();
+	int height = mem.getHeight();
+	int BPSyst = mem.getBPSyst();
+	int BPDias = mem.getBPDias();
+	String cancer = mem.getCancer();
+	String alzheimers = mem.getAlzheimers();
+	String diabetes = mem.getDiabetes();
+	ArrayList<InsuranceScore> allScores = new ArrayList<InsuranceScore>();
+	
+	//int  average = (int)total / iScore.size();
+	//String letter = familyDiseaseScore(average) + bloodPressureScore(average);
+	return age;
 }
 
 }
-/*
-Age:
-< 30
--
-0 points
-< 45
--
-10 points
-< 60
--
-20 points
-else
--
-30 points
-BMI:
-normal
--
-0 points
-overweight
--
-30 points
-else
--
-75 points
-Blood Pressure:
-normal
--
-0 points
-elevated
--
-15 points
-stage 1
--
-30 points
-stage 2
--
-75 points
-crisis
--
-100 points
-Family Disease:
-diabetes
--
-10 points
-cancer
--
-10 points
-alzheimers
--
-10 points
-
-<= 20
--
-low risk
-<= 50
--
-moderate risk
-<= 75
--
-high risk
-else
--
-uninsurable
-
-
-*/
